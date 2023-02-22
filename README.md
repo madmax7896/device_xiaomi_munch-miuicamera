1fcaa078a51a6297c1fc831a0918640075a115f5
Aryaman Srivastava <aryamansrivastava895@gmail.com>
2023-02-23 01:11:25 +0530

    munch-miuicamera: Adapt changes for munch

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# android_device_xiaomi_lisa-miuicamera
+# android_device_xiaomi_munch-miuicamera
 
-Prebuilt stock MIUI Camera for Xiaomi 11 Lite NE (lisa), to include in custom ROM builds.
+Prebuilt stock MIUI/Leica Camera for Poco F4/Redmi K40S (munch), to include in custom ROM builds.
 
-Extracted from lisa MIUI package (refer proprietary-files.txt for version).
+Extracted from munch MIUI package (refer proprietary-files.txt for version).
 
 ### How to use?
 
-1. Clone this repo to `device/xiaomi/lisa-miuicamera`
+1. Clone this repo to `device/xiaomi/munch-miuicamera`
 
 2. Inherit it from `device.mk` in device tree:
 
 ```
 # Camera
-$(call inherit-product-if-exists, device/xiaomi/lisa-miuicamera/config.mk)
+$(call inherit-product-if-exists, device/xiaomi/munch-miuicamera/config.mk)
 ```
 
 3. Ensure that required properties are set:
--- config.mk
+++ config.mk
@@ -6,22 +6,27 @@
 
 # Namespace
 PRODUCT_SOONG_NAMESPACES += \
-    device/xiaomi/lisa-miuicamera
+    device/xiaomi/munch-miuicamera
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    device/xiaomi/lisa-miuicamera/configs/permissions/default-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/default-permissions-miuicamera.xml \
-    device/xiaomi/lisa-miuicamera/configs/permissions/miuicamera-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/miuicamera-hiddenapi-package-whitelist.xml \
-    device/xiaomi/lisa-miuicamera/configs/permissions/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miuicamera.xml
+    device/xiaomi/munch-miuicamera/configs/permissions/default-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/default-permissions-miuicamera.xml \
+    device/xiaomi/munch-miuicamera/configs/permissions/miuicamera-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/miuicamera-hiddenapi-package-whitelist.xml \
+    device/xiaomi/munch-miuicamera/configs/permissions/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miuicamera.xml
+
+# Device-Features
+PRODUCT_COPY_FILES += \
+     device/xiaomi/munch-miuicamera/configs/device_features/munch.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/device_features/munch.xml \
+     device/xiaomi/munch-miuicamera/configs/device_features/munch.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/device_features/munch.xml
 
 # Properties
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.com.google.lens.oem_camera_package=com.android.camera \
     ro.miui.notch=1 \
-    ro.product.mod_device=lisa
+    ro.product.mod_device=munch
 
 # Sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/xiaomi/lisa-miuicamera/sepolicy/vendor
+    device/xiaomi/munch-miuicamera/sepolicy/vendor
 
-$(call inherit-product, vendor/xiaomi/lisa-miuicamera/lisa-miuicamera-vendor.mk)
+$(call inherit-product, vendor/xiaomi/munch-miuicamera/munch-miuicamera-vendor.mk)
